efacdd10a7db70da56efcd128f3ba475e707f04c
Kishan Patel <imkishan96@gmail.com>
2020-10-22 23:09:37 -0500

    added tf node, fixed orientation , PASS

--- src/rs_pcl_node.cpp
+++ src/rs_pcl_node.cpp
@@ -13,8 +13,10 @@
 #include <pcl/segmentation/sac_segmentation.h>
 #include <pcl/filters/extract_indices.h>
 #include <pcl/conversions.h>
+#include <visualization_msgs/Marker.h>
+#include <pcl/common/centroid.h>
 
-ros::Publisher pub;
+ros::Publisher pub,pub_pn;
 pcl::PassThrough<pcl::PCLPointCloud2> pass, pass_1;
 pcl::PCLPointCloud2 *cloud_filtered = new pcl::PCLPointCloud2;
 pcl::PCLPointCloud2 *cloud_p = new pcl::PCLPointCloud2;
@@ -32,21 +34,24 @@ pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
 pcl::SACSegmentation<pcl::PointXYZ> seg;
 pcl::ExtractIndices<pcl::PointXYZ> extract;
 
+visualization_msgs::Marker marker;
+Eigen::Vector4f plane_center;
+
 void cloud_cb(const pcl::PCLPointCloud2ConstPtr &cloud)
 { 
   pass.setInputCloud(cloud);
   pass.setFilterFieldName("x");
-  pass.setFilterLimits(-0.5, 0.5);
+  pass.setFilterLimits(-1.5, 1.5);
   pass.filter(*cloud_filtered);
 
   pass_1.setInputCloud(cloud_filtered_ptr);
   pass_1.setFilterFieldName("z");
-  pass_1.setFilterLimits(0, 2);
+  pass_1.setFilterLimits(0, 3);
   pass_1.filter(*cloud_filtered);
 
   // seg.segment(*inliers, *coefficients);
   sor.setInputCloud(cloud_filtered_ptr);
-  sor.setLeafSize(0.1f, 0.1f, 0.1f);
+  sor.setLeafSize(0.05f, 0.05f, 0.05f);
   sor.filter(*cloud_filtered);
 
   pcl::fromPCLPointCloud2( *cloud_filtered, *cloud_pcl_ptr);
@@ -63,9 +68,33 @@ void cloud_cb(const pcl::PCLPointCloud2ConstPtr &cloud)
   extract.setNegative (false);
   extract.filter(*cloud_pcl_f_ptr);
 
+  pcl::compute3DCentroid(*cloud_pcl_ptr, *inliers, plane_center);
+
+  marker.header.frame_id = "/camera_depth_optical_frame";
+  marker.header.stamp = ros::Time();
+  marker.ns = "my_namespace";
+  marker.id = 0;
+  marker.type = visualization_msgs::Marker::ARROW;
+  marker.action = visualization_msgs::Marker::ADD;
+
+  marker.pose.position.x = 0;
+  marker.pose.position.y = 0;
+  marker.pose.position.z = 0;
+  marker.pose.orientation.x = coefficients->values[0];
+  marker.pose.orientation.y = coefficients->values[1];
+  marker.pose.orientation.z = coefficients->values[2];
+  marker.pose.orientation.w = coefficients->values[3];
+  marker.scale.x = 1;
+  marker.scale.y = 0.1;
+  marker.scale.z = 0.1;
+  marker.color.a = 1.0; 
+  marker.color.r = 0.0;
+  marker.color.g = 1.0;
+  marker.color.b = 0.0;
+
   pcl::toPCLPointCloud2(*cloud_pcl_f,*cloud_p);
   pub.publish(*cloud_p);
-  //pub.publish(*cloud_filtered);
+  pub_pn.publish(marker);
 }
 
 int main(int argc, char **argv)
@@ -79,7 +108,7 @@ int main(int argc, char **argv)
 
   // Create a ROS publisher for the output point cloud
   pub = nh.advertise<pcl::PCLPointCloud2>("output", 1);
-
+  pub_pn = nh.advertise<visualization_msgs::Marker>("plane_normal", 1);
   // Spin
   ros::spin();
 }
---
+++ src/tf.cpp
@@ -0,0 +1,21 @@
+#include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+
+int main(int argc, char** argv){
+  ros::init(argc, argv, "tf_imu_base");
+  ros::NodeHandle node;
+
+  tf::TransformBroadcaster br;
+  tf::Transform transform;
+  tf::Quaternion q;
+
+  ros::Rate rate(10.0);
+  while (node.ok()){
+    transform.setOrigin( tf::Vector3(0.0, 0.0, 0.0) );
+    q.setRPY(-1.57,0.0,-1.57);
+    transform.setRotation( q );
+    br.sendTransform(tf::StampedTransform(transform.inverse(), ros::Time::now(), "camera_imu_optical_frame", "base_link"));
+    rate.sleep();
+  }
+  return 0;
+};
